ab705bb15838b86d8eaca88c5b9c36a51b70fd10
Divesh Kumar <104316863+sherdilll@users.noreply.github.com>
2024-10-12 18:35:43 +0530

    Create reverseString.js

---
+++ reverseString.js
@@ -0,0 +1,10 @@
+// Function to reverse string
+function reverseString(str) {
+    const strRev =  str.split('').reverse().join('');
+    console.log(strRev);
+}
+
+// Function call
+reverseString("GeeksforGeeks");
+reverseString("JavaScript");
+reverseString("TypeScript");
